67efc560a2f409b18a9d303046452aa4924372ec
liqian20 <liqian20@baidu.com>
2017-09-13 15:18:10 +0800

    删除代码中的es6语法

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Layer from './components/layer/layer.js';
 import './css/common.css';
 
-const App = () => {
+const App = function () {
     var dom = document.getElementById('app');
     var layer = new Layer();
 
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,6 +3,7 @@ const common = require('./webpack.config.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
+  devtool: '#cheap-module-source-map',
   plugins: [
       new UglifyJSPlugin()
   ]
